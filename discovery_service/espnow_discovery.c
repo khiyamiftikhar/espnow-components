6f0955f801151d1a3f45d95f26cceb543478724f
khiyam <khiyamiftikhar@gmail.com>
2025-09-01 14:44:40 +0500

    The timer callback is decoupled by creating a discovery task
    The timer callback notifies it on when it invokes
    And passes it argument 0 or 1 telling whether discovery duration arrived or notifies
    
    If it is just another timer elaspe after a duration (discovery interval), the task
    again sends the discovery broadcast
    Otherwise it stops sending the broadcast and stops the timer

--- discovery_service/espnow_discovery.c
+++ discovery_service/espnow_discovery.c
@@ -1,11 +1,27 @@
 
+
+#include "string.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
 #include "esp_log.h"
 #include "espnow_discovery.h"
 #include "inttypes.h"
 
 #define         DISCOVERY_DURATION          5000    //ms
+#define         DISCOVERY_MAX_DEVICES       5
 
 static const char* TAG= "discovery";
+
+
+typedef struct {
+    uint8_t mac[6];
+}discovery_device_t;
+
+typedef struct {
+    discovery_device_t devices[DISCOVERY_MAX_DEVICES];
+    uint8_t result_count;
+}discovery_result_t;
 
 
 static struct{
@@ -14,12 +30,15 @@ static struct{
     ///Interval between each discoovery broadcast. Must be less than discovery_duration
     uint32_t discovery_interval;             //milliseconds
     uint32_t discovery_start_time;
+    TaskHandle_t discovery_task;
     //These are the interfaces that it uses and are injected to it
     discovery_comm_interface_t* message_interface;
     discovery_whitelist_interface_t* whitelist;
     discovery_timer_interface_t* timer;
     //These are the callbacks that are assigned internally and externally to appropriate invokers
     discovery_service_interface_t discovery_callback_handlers;
+    discovery_result_t discovery_result;
+    bool discovery_state;
 }discovery_service={0};
 
 
@@ -39,6 +58,11 @@ static void incoming_discovery_handler(const uint8_t *mac_addr){
         discovery_service.message_interface->acknowledge_the_discovery(mac_addr);
     }
 
+    //if(discovery_service.message_interface->is_peer_exist(mac_addr)!=true)
+      //  discovery_service.message_interface.
+
+    
+
 }
 
 
@@ -46,6 +70,9 @@ static void incoming_discovery_handler(const uint8_t *mac_addr){
 /// @param mac_addr 
 /// @return 
 static void discovery_acknowledgement_handler(const uint8_t *mac_addr){
+
+    memcpy(discovery_service.discovery_result.devices->mac,mac_addr,6);
+    discovery_service.discovery_result.result_count++;
     if(discovery_service.whitelist->is_white_listed(mac_addr)==true){
             //if yes then add as peer
             discovery_service.message_interface->add_peer(mac_addr);
@@ -57,6 +84,13 @@ static void discovery_acknowledgement_handler(const uint8_t *mac_addr){
 }
 
 
+static void stop_discovery(){
+    discovery_service.timer->stop_timer();
+    //Must be guarded with a mutex lock
+    discovery_service.discovery_state=false;
+
+}
+
 /// @brief Invoked after discovery interval
 static  void timer_elapsed_handler(){
 
@@ -65,22 +99,55 @@ static  void timer_elapsed_handler(){
     uint32_t discovery_start_time=discovery_service.discovery_start_time;
     uint32_t duration=discovery_service.discovery_duration;
     //If time passed is less than the discovery duration than 
-ESP_LOGI(TAG, "current time %"PRIu32" previous time %"PRIu32 "duration%"PRIu32, current_time, discovery_start_time,duration); 
-   if((current_time-discovery_start_time)<duration){
-        //Again send discovery broadcast message
-        discovery_service.message_interface->send_discovery();
-    }
-    else
-        discovery_service.timer->stop_timer();
+    ESP_LOGI(TAG, "current time %"PRIu32" previous time %"PRIu32 "duration%"PRIu32, current_time, discovery_start_time,duration); 
 
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    BaseType_t notify_result;
+    
+    //uint32_t result=total_devices_found;
+
+    if((current_time-discovery_start_time)<duration){
+    
+        notify_result = xTaskNotifyFromISR(discovery_service.discovery_task, 1, 
+                                          eSetValueWithOverwrite, 
+                                          &xHigherPriorityTaskWoken);
+    }
+
+    else{
+        notify_result = xTaskNotifyFromISR(discovery_service.discovery_task, 0, 
+                                          eSetValueWithOverwrite, 
+                                          &xHigherPriorityTaskWoken);
+        }
+
+    
+    /*
+    if (notify_result != pdPASS) {
+        // Notification failed - set a backup flag
+        process_failed_flag = true;
+        backup_result = result;
+    }
+    */
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+       
 }
 
+/// @brief Just Starts the timer and sets the current time in the state variable, 
+///         So that discovey can be run for a set time duration
 
 void start_discovery(){
 
+
+    //Set the record to 0
+
+    //Allready running
+    if(discovery_service.discovery_state==true)
+        return;
+
+    memset(&discovery_service.discovery_result,0,sizeof(discovery_result_t));
+    
     if(discovery_service.message_interface!=NULL && discovery_service.timer!=NULL ){
         //start discovery
-        discovery_service.message_interface->send_discovery();
+        //discovery_service.message_interface->send_discovery();
         //start timer
         ESP_LOGI(TAG,"timer interval %"PRIu32,discovery_service.discovery_interval);
         discovery_service.timer->start_timer(discovery_service.discovery_interval);
@@ -88,12 +155,39 @@ void start_discovery(){
         discovery_service.discovery_start_time=discovery_service.timer->get_current_time();
     
     }
-
+    discovery_service.discovery_state=true;
 }
 
+/// @brief Return the total number of devices discovered. This variable set to 0 on start discovery
+/// @return 
+esp_err_t discovered_devices_count(){
+    return discovery_service.discovery_result.result_count;
+}
+
+/// @brief //If task notify comes with 1 , then send discovery, else stop discovery
+/// @param args 
 
 
+static void discovery_task(void* args){
 
+    uint32_t  keep_alive=0;
+    //If task notify comes with 1 , then send discovery
+    //else stop discovery
+    while(1){
+         if(xTaskNotifyWait(0, 0, &keep_alive, portMAX_DELAY)==pdTRUE){
+            if(keep_alive==1)
+                discovery_service.message_interface->send_discovery();
+            else{
+                stop_discovery();
+                if(discovery_service.message_interface->process_discovery_completion_callback!=NULL){
+                    //Call the callback with 
+                    discovery_service.message_interface->process_discovery_completion_callback(discovery_service.discovery_result.result_count);
+                }
+            }
+        }
+    }
+
+}
 
 
 
@@ -102,6 +196,10 @@ discovery_service_interface_t* discovery_service_init(config_espnow_discovery* c
 
     if(config==NULL)
         return NULL;
+
+    BaseType_t ret=xTaskCreate(discovery_task,"discovery task",4096,NULL,5,&discovery_service.discovery_task);
+
+    ESP_ERROR_CHECK(ret!=1);
 
     discovery_service.message_interface=config->discovery;
     
